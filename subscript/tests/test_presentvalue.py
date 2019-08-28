3a330b84cfcf7d799bd294f71389341ed8a3d6ef
Håvard Berland <havb@equinor.com>
2019-08-28 15:21:04 +0200

    Make path to test data always work

--- subscript/tests/test_presentvalue.py
+++ subscript/tests/test_presentvalue.py
@@ -1,16 +1,20 @@
 import pytest  # noqa: F401
+import os
 import sys
 
 import presentvalue
 
 
 def test_main():
-    # Ouch: Only works when cwd is subscript/subscript. Fixme.
     sys.argv = [
         "presentvalue",
         "--discountto",
         "2001",
-        "tests/data/reek/eclipse/model/2_R001_REEK-0.DATA",
+        os.path.join(
+            os.path.dirname(__file__), "data/reek/eclipse/model/2_R001_REEK-0.DATA"
+        ),
     ]
+    # We only test that the script actually runs
     presentvalue.main()
+    # If stdout is captured, we could do this:
     # assert "PresentValue 11653.94" in stdout  # stdout not captured yet, fixme.
