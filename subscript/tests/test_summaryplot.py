44afbfae628745f440b5e1d55fcd7a45385d005b
Eivind Smoergrav <eism@statoil.com>
2019-10-10 16:30:30 +0200

    Porting summaryplot from SVN

---
+++ subscript/tests/test_summaryplot.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+
+from __future__ import absolute_import
+from .. import summaryplot
+import os
+import unittest
+
+
+class TestSummaryplot(unittest.TestCase):
+    def test_summaryplot(self):
+
+        datafn = os.path.join(
+            os.path.dirname(__file__), "data/reek/eclipse/model/2_R001_REEK-0.DATA"
+        )
+        args = ["./blabla", datafn, "FOPR", "-d"]
+        summaryplot.summaryplotter(*args)
+        pngfn = "summaryplotdump.png"
+        pdffn = "summaryplotdump.pdf"
+
+        assert os.path.exists(pngfn)
+        assert os.path.exists(pdffn)
+
+        if os.path.exists(pngfn):
+            os.unlink(pngfn)
+
+        if os.path.exists(pdffn):
+            os.unlink(pdffn)
+
+
+if __name__ == "__main__":
+    unittest.main()
