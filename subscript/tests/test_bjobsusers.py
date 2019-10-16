409b84b481eeca37e7f601e9f943a9eafaa3de5c
Håvard Berland <havb@equinor.com>
2019-10-16 09:22:33 +0200

    Add test of real bjobs command for interactive tests

--- subscript/tests/test_bjobsusers.py
+++ subscript/tests/test_bjobsusers.py
@@ -1,17 +1,37 @@
 import pandas as pd
 
+import pytest
+
 from .. import bjobsusers
 
 
 def fake_bjobs(status):
     return "foobar 3*computenode1\nfoobert 1*computenode2\nfoobar 8*computenode3"
 
+
 def bjobs_errors(status):
     # Example error message from bjobs
     return "LIM not responding"
 
+
 def fake_finger(username):
     return "Login: {}          Name: Foo Barrer (foo.bar.com)"
+
+
+def test_real_bjobs():
+    """Test the real bjobs command. Can only be expected to
+    work on production system interactively"""
+    jobs_df = bjobsusers.get_jobs("RUN", bjobsusers.call_bjobs)
+    if jobs_df.empty:
+        pytest.skip("bjobs command not available, skipping test")
+    assert isinstance(jobs_df, pd.DataFrame)
+    assert "ncpu" in jobs_df.columns
+    assert "user" in jobs_df.index.name
+
+    # The real bjobs is allowed to return empty..
+    if not jobs_df.empty:
+        assert len(jobs_df.index.unique()) == len(jobs_df)
+        assert jobs_df["ncpu"].sum() > 0
 
 
 def test_get_jobs():
