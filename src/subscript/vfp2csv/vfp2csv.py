27256b88b633c63ff2f13effd20867a5609916ba
Håvard Berland <havb@equinor.com>
2019-11-26 22:06:05 +0100

    Port resscript vfp2csv to subscript - untested

--- src/subscript/vfp2csv/vfp2csv.py
+++ src/subscript/vfp2csv/vfp2csv.py
@@ -1,55 +1,75 @@
-#!/usr/bin/env python
-#
-# vfp2csv
-#
-# Usage:
-#   vfp2csv vfpfile1 [vfpfile2] ... -o vfpfiles.csv
-#
-# Parses VFPPROD/VFPINJE files and dumps as CSV files
-#
-# Havard Berland, July 2016, havb@statoil.com
-#
-# Inspired by PlotVFP (in ResScript)
+"""
+Parses VFPPROD/VFPINJE files and dumps as CSV files
 
+Ad-hoc python parsing of the Eclipse 100 input files, cannot
+deal with the whole deck.
+"""
 
-import numpy
+from __future__ import print_function
+
 import argparse
-import pandas
 
-import resscript.header as header
+import numpy as np
+import pandas as pd
 
-header.compose("vfp2csv",
-               "July 2016",
-               ["Havard Berland"],
-               ["havb@statoil.com"],
-               ["Access help with -h"],
-               "Convert VFP (lift curves) into CSV files")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("vfpfiles", nargs="+",
-                    help="Text files containing VFPPROD/VFPINJE")
-parser.add_argument("-o", "--output", type=str,
-                    help="Name of output CSV file", default="vfptables.csv")
+def get_parser():
+    """Set up parser for command line utility"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "vfpfiles", nargs="+", help="Text files containing VFPPROD/VFPINJE"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Name of output CSV file",
+        default="vfptables.csv",
+    )
+    return parser
 
-args = parser.parse_args()
 
-# List of dataframes for each VFP table
-vfpframes = []
+def main():
+    """Process command line arguments"""
+    parser = get_parser()
+    args = parser.parse_args()
 
-for filename in args.vfpfiles:
+    # List of dataframes for each VFP table
+    vfpframes = []
 
-    print("Processing file " + filename)
+    for filename in args.vfpfiles:
+        print("Processing file {}".format(filename))
+        vfpframes.append(vfpfile2df(filename))
+    allvfpdata = pd.concat(vfpframes, sort=False)
+    print("Exporting to " + args.output)
+    allvfpdata.to_csv(args.output, index=False)
 
-    lines = [l.strip() for l in open(filename).readlines()
-             if not l.strip().startswith('--')]
+
+def vfpfile2df(filename):
+    """Process a VFP file (typically outputted from Prosper or similar)
+    and organize the data into a dataframe"""
+    try:
+        lines = [
+            l.strip()
+            for l in open(filename).readlines()
+            if not l.strip().startswith("--")
+        ]
+    except UnicodeDecodeError:
+        lines = [
+            l.strip()
+            for l in open(filename, encoding="iso-8859-1").readlines()
+            if not l.strip().startswith("--")
+        ]
+
     # BUG: Will not tolerate comments that has something else than a whitespace
     #      in front of it
-    vfptabledata = [a.strip() for a in " ".join(lines).split('/')]
+    vfptabledata = [a.strip() for a in " ".join(lines).split("/")]
 
     # Now loop over vfptabledata. Lines starting with VFP{PROD/INJE} are the
     # interesting ones, and the consecutive lines after.
-    vfpstartindices = [x for x in range(0, len(vfptabledata))
-                       if vfptabledata[x].startswith("VFP")]
+    vfpstartindices = [
+        x for x in range(0, len(vfptabledata)) if vfptabledata[x].startswith("VFP")
+    ]
 
     print(" - found " + str(len(vfpstartindices)) + " vfp keywords")
     for vfptableidx in vfpstartindices:
@@ -63,16 +83,15 @@ for filename in args.vfpfiles:
             if len(vfptabledata[vfptableidx].split()) > 6:
                 thp = vfptabledata[vfptableidx].split()[6].replace("'", "")
             else:
-                thp = "THP" # Default in E100
+                thp = "THP"  # Default in E100
             if len(vfptabledata[vfptableidx].split()) > 7:
-                alq = vfptabledata[vfptableidx].split()[7]\
-                                               .replace("'", "").strip()
+                alq = vfptabledata[vfptableidx].split()[7].replace("'", "").strip()
             else:
-                alq = "" # E100 default
+                alq = ""  # E100 default
             if len(vfptabledata[vfptableidx].split()) > 8:
                 units = vfptabledata[vfptableidx].split()[8].replace("'", "")
             else:
-                units = "default" # Default unit is defined in DATA file
+                units = "default"  # Default unit is defined in DATA file
             if len(vfptabledata[vfptableidx].split()) > 9:
                 tab = vfptabledata[vfptableidx].split()[9].replace("'", "")
             else:
@@ -83,54 +102,50 @@ for filename in args.vfpfiles:
             tab = "BHP"
             shift = 3
 
-        flow_values = numpy.array(vfptabledata[vfptableidx+1]\
-                                  .split()).astype(float)
-        thp_values = numpy.array(vfptabledata[vfptableidx+2]\
-                                 .split()).astype(float)
+        flow_values = np.array(vfptabledata[vfptableidx + 1].split()).astype(float)
+        thp_values = np.array(vfptabledata[vfptableidx + 2].split()).astype(float)
 
         if vfptype == "VFPPROD":
-            wfr_values = numpy.array(vfptabledata[vfptableidx+3]\
-                                     .split()).astype(float)
-            gfr_values = numpy.array(vfptabledata[vfptableidx+4]\
-                                     .split()).astype(float)
-            alq_values = numpy.array(vfptabledata[vfptableidx+5]\
-                                     .split()).astype(float)
+            wfr_values = np.array(vfptabledata[vfptableidx + 3].split()).astype(float)
+            gfr_values = np.array(vfptabledata[vfptableidx + 4].split()).astype(float)
+            alq_values = np.array(vfptabledata[vfptableidx + 5].split()).astype(float)
 
         if vfptype == "VFPPROD":
-            rows = len(alq_values) * len(thp_values) \
-                   * len(wfr_values) * len(gfr_values)
+            rows = len(alq_values) * len(thp_values) * len(wfr_values) * len(gfr_values)
         elif vfptype == "VFPINJ":
             rows = len(thp_values)
 
-
-        bhp_values = pandas.DataFrame(
-            [list(map(float, x.split()))\
-             for x in vfptabledata[vfptableidx+shift:vfptableidx+shift+rows]])
+        bhp_values = pd.DataFrame(
+            [
+                list(map(float, x.split()))
+                for x in vfptabledata[vfptableidx + shift : vfptableidx + shift + rows]
+            ]
+        )
 
         # Replace the indices in the first four columns with
         # the actual values they represent:
         if vfptype == "VFPPROD":
-            bhp_values[0] = [thp_values[int(x)-1] for x in bhp_values[0]]
-            bhp_values[1] = [wfr_values[int(x)-1] for x in bhp_values[1]]
-            bhp_values[2] = [gfr_values[int(x)-1] for x in bhp_values[2]]
-            bhp_values[3] = [alq_values[int(x)-1] for x in bhp_values[3]]
+            bhp_values[0] = [thp_values[int(x) - 1] for x in bhp_values[0]]
+            bhp_values[1] = [wfr_values[int(x) - 1] for x in bhp_values[1]]
+            bhp_values[2] = [gfr_values[int(x) - 1] for x in bhp_values[2]]
+            bhp_values[3] = [alq_values[int(x) - 1] for x in bhp_values[3]]
         elif vfptype == "VFPINJ":
-            bhp_values[0] = [thp_values[int(x)-1] for x in bhp_values[0]]
+            bhp_values[0] = [thp_values[int(x) - 1] for x in bhp_values[0]]
 
         # Set up column names to allow for stacking the flow values
         # (wide data to long data)
         if vfptype == "VFPPROD":
             if alq:
-                alq = '-' + alq
-            indextuples = [(thp, ''), (wfr, ''), (gfr, ''), ('ALQ' + alq, '')]
+                alq = "-" + alq
+            indextuples = [(thp, ""), (wfr, ""), (gfr, ""), ("ALQ" + alq, "")]
         elif vfptype == "VFPINJ":
-            indextuples = [('THP', '')]
+            indextuples = [("THP", "")]
 
         for flowvalue in flow_values:
-            indextuples.append(('BHP', flowvalue))
+            indextuples.append(("BHP", flowvalue))
 
         # Set the columns to a MultiIndex, to facilitate stacking
-        bhp_values.columns = pandas.MultiIndex.from_tuples(indextuples)
+        bhp_values.columns = pd.MultiIndex.from_tuples(indextuples)
 
         # Now stack
         bhp_values_stacked = bhp_values.stack()
@@ -142,14 +157,13 @@ for filename in args.vfpfiles:
         bhp_values_stacked.bfill(inplace=True)
         # Also reset the index:
         bhp_values_stacked.reset_index(inplace=True)
-        bhp_values_stacked.drop('level_0', axis='columns', inplace=True)
+        bhp_values_stacked.drop("level_0", axis="columns", inplace=True)
         # This column is not meaningful (it is the old index)
 
         # Delete rows that does not belong to any flow rate (this is
         # possibly a by-product of not doing the stacking in an
         # optimal way)
-        bhp_values_stacked = bhp_values_stacked[
-            bhp_values_stacked['level_1'] != '']
+        bhp_values_stacked = bhp_values_stacked[bhp_values_stacked["level_1"] != ""]
 
         # Add correct column name for the flow values that we have stacked
         cols = list(bhp_values_stacked.columns)
@@ -157,17 +171,13 @@ for filename in args.vfpfiles:
         bhp_values_stacked.columns = cols
 
         # Add meta-data
-        bhp_values_stacked['VFPTYPE'] = vfptype
-        bhp_values_stacked['TABLENUMBER'] = int(tableno)
-        bhp_values_stacked['DATUM'] = float(datum)
+        bhp_values_stacked["VFPTYPE"] = vfptype
+        bhp_values_stacked["TABLENUMBER"] = int(tableno)
+        bhp_values_stacked["DATUM"] = float(datum)
         if vfptype == "VFPPROD":
-            bhp_values_stacked['UNITS'] = units
-            bhp_values_stacked['TABTYPE'] = tab
+            bhp_values_stacked["UNITS"] = units
+            bhp_values_stacked["TABTYPE"] = tab
 
-        bhp_values_stacked['filename'] = filename
+        bhp_values_stacked["FILENAME"] = filename
 
-        vfpframes.append(bhp_values_stacked)
-
-allvfpdata = pandas.concat(vfpframes, sort=False)
-print("Exporting to " + args.output)
-allvfpdata.to_csv(args.output, index=False)
+        return bhp_values_stacked
