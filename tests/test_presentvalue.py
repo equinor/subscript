08d49827f8d559dc0d2a2bf150ba4d50192ef260
Håvard Berland <havb@equinor.com>
2020-04-24 11:13:51 +0200

    Avoid polluting test dir in presentvalue testing

--- tests/test_presentvalue.py
+++ tests/test_presentvalue.py
@@ -7,17 +7,31 @@ import shutil
 
 import subprocess
 import pytest
+import shutil
+import six
 
 from subscript.presentvalue import presentvalue
+
+ECLDIR = os.path.join(os.path.dirname(__file__), "data/reek/eclipse/model")
 
 
 def test_main(tmpdir):
     """Test the main functionality of presentvalue as endpoint script, writing
     back results to parameters.txt in the original runpath"""
-    ecldir = os.path.join(os.path.dirname(__file__), "data/reek/eclipse/model")
     tmpdir.chdir()
-    shutil.copytree(ecldir, "model")
+    shutil.copytree(
+        ECLDIR,
+        "model"
+        # This is somewhat spacious, 39M, but the test will fail
+        # if you try with a symlink (presentvalue.py looks through symlinks)
+    )
+    tmpdir.join("model").chdir()
+
     parameterstxt_fname = "parameters.txt"
+
+    # Remove the potential copy we have got in our tmpdir:
+    if os.path.exists(parameterstxt_fname):
+        os.unlink(parameterstxt_fname)
 
     # Create an empty file called parameters.txt, otherwise
     # the presentvalue script will not write to it.
@@ -28,7 +42,7 @@ def test_main(tmpdir):
         "--writetoparams",
         "--discountto",
         "2001",
-        os.path.join("model", "2_R001_REEK-0.DATA"),
+        "2_R001_REEK-0.DATA",
     ]
     presentvalue.main()
     parametersline = open(parameterstxt_fname).readlines()[0].strip()
