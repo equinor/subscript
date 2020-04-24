58fb69b12f2ab53aae4b69dcc15958e8f640dbb6
Thomas Ramstad <trams@statoil.com>
2020-04-24 15:42:26 +0200

    Updated test script 	modified:   tests/test_sector2fluxnum.py

--- tests/test_sector2fluxnum.py
+++ tests/test_sector2fluxnum.py
@@ -4,32 +4,30 @@ import subprocess
 import shlex
 import shutil
 import os
+import pytest
 
-from ert.test import TestAreaContext
+from subscript.sector2fluxnum import sector2fluxnum
 
 def test_sector2fluxnum():
     input_path = "data/sector"
     root = "/private/trams/GitHub/subscript"
-#    with TestAreaContext("Coarse_FLUX_generation"):
     script = "src/subscript/sector2fluxnum/sector2fluxnum.py"
     
     input_ECL_CASE = os.path.join(input_path, "TEST.DATA")
     input_OUTPUT_FLUX = os.path.join(input_path, "OUT_COARSE.FLUX")
-    input_RESTART = "-r" + os.path.join(input_path, "TEST.UNRST")
-    input_i = "-i 2-10"
-    input_j = "-j 2-9"
-    input_k = "-k 1-1"
+    input_INPUT_DUMPFLUX = os.path.join(input_path, "DUMPFLUX_TEST.DATA")
+    input_RESTART = os.path.join(input_path, "TEST.UNRST")
     
-    p = subprocess.check_call(['python',
-                               os.path.join(root, script),
-                               input_ECL_CASE,
-                               input_OUTPUT_FLUX,
-                               input_i,
-                               input_j,
-                               input_k,
-                               input_RESTART])
+    sys.argv = ["sector2fluxnum",
+                "-i", "2-10",
+                "-j", "2-9",
+                "-k", "1-1",
+                "-r", input_RESTART,
+                "--test", input_INPUT_DUMPFLUX, 
+                input_ECL_CASE, 
+                input_OUTPUT_FLUX]
     
-    #(output, err) = p.communicate()
-    #assertFalse(err)
+    sector2fluxnum.main()
+
     assert(os.path.isfile("USEFLUX_TEST.DATA")) 
     
