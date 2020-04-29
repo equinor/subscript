82a340d5ac7bb6b2450f76028785e5a608720cf3
Thomas Ramstad <trams@statoil.com>
2020-04-29 18:01:07 +0200

    black test_sector2fluxnum.py 	modified:   tests/test_sector2fluxnum.py

--- tests/test_sector2fluxnum.py
+++ tests/test_sector2fluxnum.py
@@ -3,38 +3,41 @@
 from __future__ import absolute_import
 
 import sys
-import subprocess
-import shlex
-import shutil
 import os
-import pytest
 
 from subscript.sector2fluxnum import sector2fluxnum
+
 
 def test_sector2fluxnum():
 
     TESTDATA = os.path.join(os.path.dirname(__file__), "data/sector")
 
-    testdir =  os.path.join(os.path.dirname(__file__), "testdata_sector2fluxnum")
+    testdir = os.path.join(os.path.dirname(__file__), "testdata_sector2fluxnum")
     if not os.path.exists(testdir):
         os.mkdir(testdir)
     os.chdir(testdir)
-    
+
     input_ECL_CASE = os.path.join(TESTDATA, "TEST.DATA")
     input_OUTPUT_FLUX = os.path.join(TESTDATA, "OUT_COARSE.FLUX")
     input_INPUT_DUMPFLUX = os.path.join(TESTDATA, "DUMPFLUX_TEST.DATA")
     input_RESTART = os.path.join(TESTDATA, "TEST.UNRST")
-    
-    sys.argv = ["sector2fluxnum",
-                "-i", "2-10",
-                "-j", "2-9",
-                "-k", "1-1",
-                "-r", input_RESTART,
-                "--test", input_INPUT_DUMPFLUX, 
-                input_ECL_CASE, 
-                input_OUTPUT_FLUX]
-    
+
+    sys.argv = [
+        "sector2fluxnum",
+        "-i",
+        "2-10",
+        "-j",
+        "2-9",
+        "-k",
+        "1-1",
+        "-r",
+        input_RESTART,
+        "--test",
+        input_INPUT_DUMPFLUX,
+        input_ECL_CASE,
+        input_OUTPUT_FLUX,
+    ]
+
     sector2fluxnum.main()
 
-    assert(os.path.isfile("USEFLUX_TEST.DATA")) 
-    
+    assert os.path.isfile("USEFLUX_TEST.DATA")
