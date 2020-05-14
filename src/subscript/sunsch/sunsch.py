560303f457de7a8e32a128c6a1287d708c52908a
Håvard Berland <havb@equinor.com>
2020-05-14 19:38:34 +0200

    Back to configsuite 0.5.3 syntax

--- src/subscript/sunsch/sunsch.py
+++ src/subscript/sunsch/sunsch.py
@@ -134,15 +134,14 @@ CONFIG_SCHEMA_V2 = {
                 }
             },
         },
-        "output": {MK.Type: types.String, MK.Required: False, MK.AllowNone: True},
-        "startdate": {MK.Type: types.Date, MK.Required: False, MK.AllowNone: True},
-        "starttime": {MK.Type: types.DateTime, MK.Required: False, MK.AllowNone: True},
-        "refdate": {MK.Type: types.Date, MK.Required: False, MK.AllowNone: True},
-        "enddate": {MK.Type: types.Date, MK.Required: False, MK.AllowNone: True},
+        "output": {MK.Type: types.String, MK.Required: False},
+        "startdate": {MK.Type: types.Date, MK.Required: False},
+        "starttime": {MK.Type: types.DateTime, MK.Required: False},
+        "refdate": {MK.Type: types.Date, MK.Required: False},
+        "enddate": {MK.Type: types.Date, MK.Required: False},
         "dategrid": {
             MK.Type: types.String,
             MK.Required: False,
-            MK.AllowNone: True,
             MK.ElementValidators: (_is_valid_dategrid,),
         },
         "insert": {
@@ -152,33 +151,19 @@ CONFIG_SCHEMA_V2 = {
                 MK.Item: {
                     MK.Type: types.NamedDict,
                     MK.Content: {
-                        "date": {
-                            MK.Type: types.Date,
-                            MK.Required: False,
-                            MK.AllowNone: True,
-                        },
+                        "date": {MK.Type: types.Date, MK.Required: False},
                         "filename": {
                             MK.Type: types.String,
                             MK.Required: False,
-                            MK.AllowNone: True,
                             MK.ElementValidators: (_is_existing_file,),
                         },
                         "template": {
                             MK.Type: types.String,
                             MK.Required: False,
-                            MK.AllowNone: True,
                             MK.ElementValidators: (_is_existing_file,),
                         },
-                        "days": {
-                            MK.Type: types.Integer,
-                            MK.Required: False,
-                            MK.AllowNone: True,
-                        },
-                        "string": {
-                            MK.Type: types.String,
-                            MK.Required: False,
-                            MK.AllowNone: True,
-                        },
+                        "days": {MK.Type: types.Integer, MK.Required: False},
+                        "string": {MK.Type: types.String, MK.Required: False},
                         "substitute": {
                             MK.Type: types.Dict,
                             MK.Required: False,
