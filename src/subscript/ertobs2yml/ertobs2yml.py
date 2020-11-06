aa839acba63561a95543432cdeb38594fe2fbce8
Håvard Berland <havb@equinor.com>
2020-11-06 08:38:20 +0100

    Split out ERT-obs-format parser into separate module

--- src/subscript/ertobs2yml/ertobs2yml.py
+++ src/subscript/ertobs2yml/ertobs2yml.py
@@ -1,10 +1,8 @@
 """Parse ERT observation files"""
 import os
-import re
 import sys
 import signal
 import logging
-import datetime
 
 import argparse
 import yaml
@@ -13,6 +11,11 @@ import numpy as np
 import pandas as pd
 
 from subscript import getLogger
+
+from subscript.ertobs2yml.ertobs_parser import (
+    lowercase_dictkeys,
+    ertobs2df,
+)
 
 logger = getLogger(__name__)
 
@@ -32,43 +35,8 @@ EXAMPLES = """
   FORWARD_MODEL ERTOBS2YML(<OBS_FILE>=observations.txt, <CSV_OUTPUT>=observations.csv, <YML_OUTPUT>=observations.yml)
 """  # noqa
 
-# Regular expressions for matching ERT observation files.
-# Note that lower-case is supported by the regexp's, but might not be supported
-# by ERT.
-_WHITESPACE = r"[\s]*"
-_KEY_VALUE_CHARS = r"\nA-Za-z:/=_\-\.,0-9\s"
-_OBS_CLASS = r"([A-Z_]+)"
-_OBS_LABEL = r"([A-Za-z0-9_\-]+)"
-_SEMICOLON = _WHITESPACE + ";" + _WHITESPACE
-
-# This avoids nested {}, and avoids capturing trailing ; in matched group
-_OPTIONAL_CURLY_SUBGROUP = r"(\{[A-Za-z=0-9\.,_;/\s]*\})?" + _SEMICOLON
-
-OBS_ARGS_RE = re.compile(
-    _WHITESPACE
-    + "(["
-    + _KEY_VALUE_CHARS
-    + "]*)"
-    + _WHITESPACE
-    + _OPTIONAL_CURLY_SUBGROUP
-)
-"""Regular expression for splitting observation arguments into a
-key-values element, and a curly-braces element. The curly braces
-can not be nested."""
-
-_QUOTE = r"[\"']*"
-_FILENAME_CHARS = r"\w\s\.\-_"
-INCLUDE_RE = re.compile(
-    r"\s*(include\s+"
-    + _QUOTE
-    + ")(["
-    + _FILENAME_CHARS
-    + "]+)("
-    + _QUOTE
-    + _WHITESPACE
-    + ";)"
-)
-"""Regular expression for capturing include statements in input file"""
+__MAGIC_NONE__ = "__NONE__"  # For ERT hook defaults support.
+__MAGIC_STDOUT__ = "-"
 
 # Used in yaml file
 CLASS_SHORTNAME = {
@@ -77,11 +45,6 @@ CLASS_SHORTNAME = {
     "BLOCK_OBSERVATION": "rft",
     "HISTORY_OBSERVATION": "hist",
 }
-
-ERT_DATE_FORMAT = "%d/%m/%Y"
-
-__MAGIC_NONE__ = "__NONE__"  # For ERT hook defaults support.
-__MAGIC_STDOUT__ = "-"
 
 
 class CustomFormatter(
@@ -128,356 +91,6 @@ def get_parser():
     return parser
 
 
-def expand_includes(input_str, cwd="."):
-    """Look for include 'filename.txt'; in the string, and replace
-    it with the contents of that file. Filenames need not be quoted,
-    but semicolons (and most other special characters) in filenames
-    will not be resolved.
-
-    Args:
-        input_str (str): String with potential include-statements.
-        cwd (str): Path to what should be used as current working directory
-            when resolving include statements (for include files). Defaults
-            to current directory, but the ERT observation format assumes
-            it is the directory of the ERT config file.
-    """
-    match = INCLUDE_RE.search(input_str)
-    while match:
-        include_filename = os.path.join(cwd, match.groups()[1])
-        logger.info(
-            "Injecting include file: %s into observation file", include_filename
-        )
-        with open(include_filename) as f_handle:
-            include_txt = f_handle.read()
-        input_str = input_str.replace("".join(match.groups()), include_txt)
-        match = INCLUDE_RE.search(input_str)
-    return input_str
-
-
-def mask_curly_braces(string, mask_char="X"):
-    """In order to support nested curly braces, a regular expression
-    cannot be used to do the outermost "split-by-semicolon" operation.
-
-    Instead we mask away the curly braces, so that all semicolons occuring
-    in the string are at the outermost level - then we can split the
-    unmasked string by the semicolon position in the masked string.
-    """
-    assert len(mask_char) == 1
-    for match in re.compile(_OPTIONAL_CURLY_SUBGROUP).findall(string):
-        if match:
-            string = string.replace(match, mask_char * len(match))
-    # When curly braces are nested, there are strings like {FOO=BAR; XXXX;} left
-    # in the strings after the loop above, mask these also:
-    for match in re.compile(
-        # r"(\{[\sA-Za-z=\.,:;0-9\-_/" + mask_char + r";]+\})"
-        r"(\{["
-        + _KEY_VALUE_CHARS
-        + mask_char
-        + r";]+\})"
-    ).findall(string):
-        if match:
-            string = string.replace(match, mask_char * len(match))
-    return string
-
-
-def split_by_sep_in_masked_string(string, masked_string, sep=";"):
-    """Splits a string by a separator, but the separators are searched
-    for in an auxiliary string, the "masked_string". This is so in order
-    to mask out separator characters that should be ignored due to
-    being enclosed in f.ex. curly braces.
-
-    Splitting::
-        "foo { bar; com}; hei hopp;"
-
-    with a masked string like::
-
-        "foo XXXXXXXXXXX; hei hopp;"
-
-    results in two components::
-
-        ["foo { bar; com}", "hei hopp"]
-
-    Args:
-        string (str): The string that should be split
-        masked_string (str): Same length as first argument, but where
-            only a subset of the separator characters need to match up.
-        sep (str): Separator character, defaults to ";"
-
-    Yields:
-        string: Each part of the input string. Separator
-        character is not included
-    """
-
-    if len(string) != len(masked_string):
-        raise ValueError("string and masked_string do not have equal length")
-
-    if not masked_string:
-        return []
-
-    # Delete trailing ";\s" from both masked_string and string
-    while masked_string[-1] == " ":
-        masked_string = masked_string[0:-1]
-        string = string[0:-1]
-    if masked_string[-1] == sep:
-        masked_string = masked_string[0:-1]
-        string = string[0:-1]
-
-    sep_positions = [-1]
-    for match in re.finditer(sep, masked_string):
-        if string[match.start()] != sep:
-            raise ValueError("string and masked_string do not match on separators")
-        if match.start() < len(string) - 1:
-            sep_positions.append(match.start())
-    return filter(
-        len,
-        (
-            string[i + 1 : j].strip()
-            for i, j in zip(sep_positions, sep_positions[1:] + [len(string)])
-        ),
-    )
-
-
-def filter_comments(input_str, comment_identifier="--"):
-    """Strip comments from a multiline string.
-
-    If the comment_identifier, defaults to "--" is found, the remainder
-    of the line is stripped. Empty lines are also dropped (being the
-    result of comment stripping or not).
-
-    Args:
-        input_str (str): String to strip comments from
-        comment_identifier (str): Character sequence that marks the rest
-            of the line as a comment.
-
-    Returns:
-        str: String with newlines where there are no comments, and
-        with no empty lines.
-    """
-    lines = input_str.split("\n")
-
-    # Drop comments
-    lines = (line.strip().split(comment_identifier)[0].strip() for line in lines)
-
-    # Drop empty lines:
-    lines = filter(len, lines)
-
-    return "\n".join(lines)
-
-
-def fix_dtype(value):
-    """Guess the correct datatype for an incoming value.
-
-    If parseable as float, return as integer if it is, return float if not.
-    If parseable as an ERT date (DD/MM/YYYY), return as datetime.date.
-
-    If not, return as string.
-
-    Args:
-        value: Object of any type given it has a string representation.
-
-    Returns:
-        int, float, datetime.date or string.
-    """
-    try:
-        int_value = int(float(value))
-        float_value = float(value)
-        if int_value == float_value:
-            return int_value
-        return float_value
-    except ValueError:
-        try:
-            return datetime.datetime.strptime(value, ERT_DATE_FORMAT)
-        except ValueError:
-            return str(value)
-
-
-def remove_enclosing_curly_braces(string):
-    """Removes enclosing curly braces around a string, permitting
-    whitespace at start and end, and also a trailing semicolon after
-    the curly brace end"""
-    if not string:
-        return string
-    string = string.strip()
-    if string[0] == "{":
-        string = string[1:].strip()
-    if string[-1] == ";":
-        string = string[:-1].strip()
-    if string[-1] == "}":
-        string = string[:-1].strip()
-    return string
-
-
-def parse_observation_unit(obsunit):
-    """Parse one observation, with all its arguments. The string should
-    be the content of the curly braces after one of SUMMARYOBSERVATION,
-    BLOCK_OBSERVATION,  GENERAL_OBSERVATION or HISTORY_OBSERVATION.
-
-    The submitted string can contain so-called observation subunits
-    within curly braces.
-
-    Args:
-        string (str): String with observation data to parse.
-
-    Returns:
-        dict: Parsed observation data. Data inside curly braces are
-        returned in nested dicts.
-    """
-    obsunit = obsunit.replace("\n", "")
-    if not obsunit:
-        return {}
-
-    obs_dict = {}
-    subunits = {}
-    # Split by semi-colons outside curly braces:
-    for obs_subunit in OBS_ARGS_RE.findall(obsunit):
-        if not obs_subunit[0]:
-            # regexp sometimes give empty matches, ignore.
-            continue
-        if "=" in obs_subunit[0]:
-            obs_dict[obs_subunit[0].split("=")[0].strip()] = fix_dtype(
-                obs_subunit[0].split("=")[1].strip()
-            )
-        else:
-            # If there is no =, it is not a key-value statement, but
-            # a subunit with data in curly braces.
-            subunits[obs_subunit[0].strip()] = parse_subobservation_args(obs_subunit[1])
-    return {**obs_dict, **subunits}
-
-
-def parse_subobservation_args(string):
-    """Parse the key=value arguments given to an observation sub-unit
-
-    This is semicolon-separated list of key-values, optionally enclosed in curly
-    braces. There should not be curly braces inside this data.
-
-    Args:
-        string (str): String to parse.
-
-    Returns:
-        dict: Parsed data. Strings are converted to numerical if possible.
-    """
-
-    keyvalues = {}
-    for keyvalue in remove_enclosing_curly_braces(string).split(";"):
-        if not keyvalue:
-            continue
-        if "=" in keyvalue:
-            key, value = keyvalue.split("=")
-            keyvalues[key.strip()] = fix_dtype(value.strip())
-    return keyvalues
-
-
-def flatten_observation_unit(obsunit, subunit_label="obs_sub_id"):
-    """Flatten/unroll a observation unit represented as a nested dict, return
-    as a list of dicts.
-
-    Example (written as in the ascii observation format)::
-
-      { FIELD=PRESSURE; OBS P1 {I=1;}; OBS P2 {I=3;}}
-
-    will be returned as::
-
-      [ {'OBS': 'P1', 'FIELD': 'PRESSURE', 'I=1'},
-        {'OBS': 'P2', 'FIELD': 'PRESSURE', 'I=3'}]
-
-    If there are overlapping key-names at each nesting level, the
-    lower nesting level wins, relevant for SEGMENT_START syntax.
-
-    If there is no nested dict, a list of length 1 is returned.
-
-    If the string "SEGMENT" (case-sensititive) is found,
-    this is a special case where we need to inject a "default"
-    segment in addition::
-
-      { ERROR=10; SEGMENT S1 { START=2; END=4; ERROR=5;};};
-
-    gives two elements::
-
-      [ {'SEGMENT': 'DEFAULT', 'ERROR': 10},
-        {'SEGMENT': 'S1',  'START': 2, 'END': 4, 'ERROR': 5} ]
-
-    Args:
-        obsunit (dict): Dictionary describing the contents of an observation.
-            (nb: it does not hold the observation type or observation id). If
-            there are "observation subunits", then this dict is nested.
-
-    Returns:
-        list: List of non-nested dictionaries.
-    """
-    if subunit_label in obsunit:
-        raise ValueError("Conflict, subunit_label cannot be in use")
-    subunit_keys = [
-        subunit_key
-        for subunit_key, subunit_value in obsunit.items()
-        if isinstance(subunit_value, dict)
-    ]
-    if not subunit_keys:
-        return [obsunit]
-    keyvalue_keys = set(obsunit.keys()) - set(subunit_keys)
-    keyvalues = {key: obsunit[key] for key in keyvalue_keys}
-    obs_subunits = []
-
-    # Inject a default segment if segments are in use:
-    if any(["SEGMENT" in key for key in subunit_keys]):
-        obs_subunits.append({**{"SEGMENT": "DEFAULT"}, **keyvalues})
-
-    for subunit in subunit_keys:
-        if len(subunit.split()) < 2:
-            # It must be two strings, like "OBS P1", or "SEGMENT FIRST_YEAR".
-            raise ValueError("Wrong observation subunit syntax: " + str(subunit))
-        obs_subunits.append(
-            {
-                **{subunit.split()[0]: subunit.split()[1]},
-                **keyvalues,
-                **obsunit[subunit],
-            }
-        )
-    return obs_subunits
-
-
-def ertobs2df(input_str, cwd="."):
-    """Parse a string with ERT observations and convert to
-    an equivalent tabular format, represented by a Pandas dataframe.
-
-    Args:
-        input_str (str): String in ERT observation syntax. Newlines and
-            comments are allowed.
-        cwd (str): Path to what should be used as current working directory
-            when resolving include statements (for include files). Defaults
-            to current directory, but the ERT observation format assumes
-            it is the directory of the ERT config file.
-
-    Returns:
-        pd.DataFrame
-    """
-    ertobs_str = expand_includes(filter_comments(input_str), cwd=cwd)
-
-    # Helper string for splitting correctly on semicolons
-    masked_string = mask_curly_braces(ertobs_str)
-    obs_list = []
-    for observation_unit_str in split_by_sep_in_masked_string(
-        ertobs_str, masked_string
-    ):
-        obs_unit_split = observation_unit_str.replace("{", " {").split()
-        # Two or three components depending on observation type.
-        if len(obs_unit_split) < 2:
-            print(observation_unit_str)
-            print(obs_unit_split)
-            raise ValueError
-        obs_unit = {"CLASS": obs_unit_split[0], "LABEL": obs_unit_split[1]}
-        logger.info("Parsing observation %s %s", obs_unit["CLASS"], obs_unit["LABEL"])
-        if len(obs_unit_split) > 2:
-            obs_args = " ".join(obs_unit_split[2:])
-            logger.debug("Subunit data: %s", str(obs_args))
-            for obs_subunit in flatten_observation_unit(
-                parse_observation_unit(obs_args)
-            ):
-                obs_list.append({**obs_unit, **obs_subunit})
-        else:
-            obs_list.append(obs_unit)
-    return pd.DataFrame(obs_list)
-
-
 def validate_dframe(obs_df):
     """Validate a dataframe of observations"""
     failed = False
@@ -508,11 +121,6 @@ def validate_dframe(obs_df):
     # general requires data, restart, obs_file. index_list, index_file,
     # error_covariance is optional.
     return not failed
-
-
-def lowercase_dictkeys(some_dict):
-    """Convert all keys in a dictionary to lowe-case"""
-    return {key.lower(): value for key, value in some_dict.items()}
 
 
 def summary_df2obsdict(smry_df):
