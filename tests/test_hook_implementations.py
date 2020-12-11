e5be143a223a553613b11291b2dfcda67d21c515
Eivind Jahren <eivind.jahren@webstep.no>
2020-12-11 14:10:57 +0100

    Use path to installed subscript in tests
    Tests of config_jobs would look up job configs in ../src,
    however, in order to test installed versions of subscript,
    this is changed to subscript.__file__.

--- tests/test_hook_implementations.py
+++ tests/test_hook_implementations.py
@@ -1,43 +1,53 @@
-import os
 import shutil
+from os import path
 
 import pytest
 import rstcheck
-
 from ert_shared.plugins.plugin_manager import ErtPluginManager
 
 import subscript.hook_implementations.jobs
 
-EXPECTED_JOBS = {
-    "CSV2OFMVOL": "subscript/config_jobs/CSV2OFMVOL",
-    "ECLCOMPRESS": "subscript/config_jobs/ECLCOMPRESS",
-    "ECLDIFF2ROFF": "subscript/config_jobs/ECLDIFF2ROFF",
-    "ECLGRID2ROFF": "subscript/config_jobs/ECLGRID2ROFF",
-    "ECLINIT2ROFF": "subscript/config_jobs/ECLINIT2ROFF",
-    "ECLRST2ROFF": "subscript/config_jobs/ECLRST2ROFF",
-    "INTERP_RELPERM": "subscript/config_jobs/INTERP_RELPERM",
-    "MERGE_RFT_ERTOBS": "subscript/config_jobs/MERGE_RFT_ERTOBS",
-    "OFMVOL2CSV": "subscript/config_jobs/OFMVOL2CSV",
-    "PRTVOL2CSV": "subscript/config_jobs/PRTVOL2CSV",
-    "SUNSCH": "subscript/config_jobs/SUNSCH",
-}
+# pylint: disable=redefined-outer-name
+
+
+@pytest.fixture
+def expected_jobs(path_to_subscript):
+    """ dictionary of installed jobs with location to config"""
+    expected_job_names = [
+        "CSV2OFMVOL",
+        "ECLCOMPRESS",
+        "ECLDIFF2ROFF",
+        "ECLGRID2ROFF",
+        "ECLINIT2ROFF",
+        "ECLRST2ROFF",
+        "INTERP_RELPERM",
+        "MERGE_RFT_ERTOBS",
+        "OFMVOL2CSV",
+        "PRTVOL2CSV",
+        "SUNSCH",
+    ]
+    return {
+        name: path.join(path_to_subscript, "config_jobs", name)
+        for name in expected_job_names
+    }
+
 
 # Avoid category inflation. Add to this list when it makes sense:
 ACCEPTED_JOB_CATEGORIES = ["modeling", "utility"]
 
 
-def test_hook_implementations():
+def test_hook_implementations(expected_jobs):
     """Test that we have the correct set of jobs installed,
     nothing more, nothing less"""
     plugin_m = ErtPluginManager(plugins=[subscript.hook_implementations.jobs])
 
     installable_jobs = plugin_m.get_installable_jobs()
-    for wf_name, wf_location in EXPECTED_JOBS.items():
+    for wf_name, wf_location in expected_jobs.items():
         assert wf_name in installable_jobs
         assert installable_jobs[wf_name].endswith(wf_location)
-        assert os.path.isfile(installable_jobs[wf_name])
+        assert path.isfile(installable_jobs[wf_name])
 
-    assert set(installable_jobs.keys()) == set(EXPECTED_JOBS.keys())
+    assert set(installable_jobs.keys()) == set(expected_jobs.keys())
 
     expected_workflow_jobs = {}
     installable_workflow_jobs = plugin_m.get_installable_workflow_jobs()
@@ -48,23 +58,21 @@ def test_hook_implementations():
     assert set(installable_workflow_jobs.keys()) == set(expected_workflow_jobs.keys())
 
 
-def test_job_config_syntax():
+def test_job_config_syntax(expected_jobs):
     """Check for syntax errors made in job configuration files"""
-    src_path = os.path.join(os.path.dirname(__file__), "../src")
-    for _, job_config in EXPECTED_JOBS.items():
+    for _, job_config in expected_jobs.items():
         # Check (loosely) that double-dashes are enclosed in quotes:
-        with open(os.path.join(src_path, job_config)) as f_handle:
+        with open(job_config) as f_handle:
             for line in f_handle.readlines():
                 if not line.strip().startswith("--") and "--" in line:
                     assert '"--' in line and " --" not in line
 
 
 @pytest.mark.integration
-def test_executables():
+def test_executables(expected_jobs):
     """Test executables listed in job configurations exist in $PATH"""
-    src_path = os.path.join(os.path.dirname(__file__), "../src")
-    for _, job_config in EXPECTED_JOBS.items():
-        with open(os.path.join(src_path, job_config)) as f_handle:
+    for _, job_config in expected_jobs.items():
+        with open(job_config) as f_handle:
             executable = f_handle.readlines()[0].split()[1]
             assert shutil.which(executable)
 
