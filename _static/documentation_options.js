1a93a0e8ee84293b29ae12bc46e274eac1c864cf
subscript-github-action <subscript-github-action>
2021-03-02 14:22:55 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.1.dev4+gb463646',
+    VERSION: '0.8.1.dev5+gc0b563a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
