9390ef92ff92e41e245b36557d4aa562815d0f2c
Håvard Berland <havb@equinor.com>
2021-03-18 16:49:05 +0100

    Split log output to stdout and stderr
    * Also fix log level bug in merge_schedule that tests uncovered.

--- tests/test_subscriptlogger.py
+++ tests/test_subscriptlogger.py
@@ -1,7 +1,9 @@
+import logging
+
 import subscript
 
 
-def test_subscriptlogger():
+def test_subscriptlogger_name():
     """Test that the subscript logger can compute a correct name for itself"""
     assert subscript.getLogger().name == "subscript"
     assert subscript.getLogger("").name == "subscript"
@@ -18,3 +20,52 @@ def test_subscriptlogger():
         subscript.getLogger("subscript.eclcompress.eclcompress.somesubmodule").name
         == "subscript.eclcompress.somesubmodule"
     )
+
+
+def test_default_logger_levels(capsys):
+    """Verify that the intended usage of this logger have expected results"""
+
+    # Scripts should start with this:
+    logger = subscript.getLogger("test_levels")
+
+    logger.debug("This DEBUG is not to be seen")
+    captured = capsys.readouterr()
+    assert "DEBUG" not in captured.out
+    assert "DEBUG" not in captured.err
+
+    logger.info("This INFO is not to be seen by default")
+    captured = capsys.readouterr()
+    assert "INFO" not in captured.out
+    assert "INFO" not in captured.err
+
+    logger.warning("This WARNING is to be seen")
+    captured = capsys.readouterr()
+    assert "WARNING" in captured.out
+    assert "WARNING" not in captured.err
+
+    logger.error("This ERROR should only be in stderr")
+    captured = capsys.readouterr()
+    assert "ERROR" not in captured.out
+    assert "ERROR" in captured.err
+
+
+def test_script_verbose_mode(capsys):
+    """Some scripts accept a --verbose option, which usually
+    mean that logging should be at INFO level"""
+    logger = subscript.getLogger("test_verbose")
+    logger.setLevel(logging.INFO)
+
+    logger.info("This INFO is to be seen")
+    captured = capsys.readouterr()
+    assert "INFO" in captured.out
+
+
+def test_script_debug_mode(capsys):
+    """Some scripts accept a --verbose option, which usually
+    mean that logging should be at INFO level"""
+    logger = subscript.getLogger("test_debug")
+    logger.setLevel(logging.DEBUG)
+
+    logger.info("This DEBUG is to be seen")
+    captured = capsys.readouterr()
+    assert "DEBUG" in captured.out
