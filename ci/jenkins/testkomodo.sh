049694bfd347cdac0bdfe5f7f8b4ffeaaf4bbdf2
Håvard Berland <havb@equinor.com>
2021-06-01 13:21:05 +0200

    Skip resinsight dev version testing by default
    Also let Komodo pick up setup.cfg for this to work in komodo testing

--- ci/jenkins/testkomodo.sh
+++ ci/jenkins/testkomodo.sh
@@ -2,3 +2,8 @@ install_test_dependencies () {
   pip install -r test_requirements.txt
   pip install -r docs_requirements.txt
 }
+
+copy_test_files () {
+    cp -r $CI_SOURCE_ROOT/tests $CI_TEST_ROOT/tests
+    cp $CI_SOURCE_ROOT/setup.cfg $CI_TEST_ROOT
+}
