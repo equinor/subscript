94c16d7db8e21e2ed3eeec0347aff85764519d95
Jan C. Rivenæs <jan@rivenaes.net>
2022-08-18 09:15:29 +0200

    Adapt to runrms migration /project/res > /prog/res

--- tests/test_runrms.py
+++ tests/test_runrms.py
@@ -275,7 +275,7 @@ PATH = os.environ["PATH"]
 PATH_PREFIX = os.environ["PATH_PREFIX"]
 if PATH.split(":")[0] != PATH_PREFIX:
     errors.append(f"PATH_PREFIX ({PATH_PREFIX}), was not prepended to PATH ({PATH})")
-if PATH_PREFIX != "/project/res/roxapi/bin":
+if PATH_PREFIX != "/prog/res/roxapi/bin":
     errors.append(f"The path for run_external is not corrent {PATH_PREFIX}")
 
 if "KOMODO_RELEASE" in os.environ:
