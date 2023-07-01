45f02a5403d16b9c486850b1e50e147bc4a6d3eb
mferrera <matt.ferrera@gmail.com>
2023-07-01 06:10:33 +0200

    Copy correct project config in testkomodo

--- ci/testkomodo.sh
+++ ci/testkomodo.sh
@@ -4,7 +4,7 @@ install_test_dependencies () {
 
 copy_test_files () {
     cp -r $CI_SOURCE_ROOT/tests $CI_TEST_ROOT/tests
-    cp $CI_SOURCE_ROOT/setup.cfg $CI_TEST_ROOT
+    cp $CI_SOURCE_ROOT/pyproject.toml $CI_TEST_ROOT
 }
 
 start_tests () {
