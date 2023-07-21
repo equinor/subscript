29e2154f35a7a670e278f671db545769a8a28e89
Alif Be <11570927+alifbe@users.noreply.github.com>
2023-07-21 11:03:39 +0200

    Add FORWARD MODEL and workflow job for params2csv (#581)
    - Add glob to parameterfile argument (needed so that it works from within ERT)
    - Add forward model PARAMS2CSV
    - Add workflow job PARAMS2CSV

--- tests/test_params2csv.py
+++ tests/test_params2csv.py
@@ -7,6 +7,14 @@ import pytest
 
 from subscript.params2csv import params2csv
 
+try:
+    # pylint: disable=unused-import
+    import ert.shared  # noqa
+
+    HAVE_ERT = True
+except ImportError:
+    HAVE_ERT = False
+
 
 def test_main(tmp_path, mocker):
     """Test invocation from command line"""
@@ -95,3 +103,91 @@ def test_spaces_in_values_single_quotes(tmp_path, mocker):
 def test_integration():
     """Test that the endpoint is installed"""
     assert subprocess.check_output(["params2csv", "-h"])
+
+
+@pytest.mark.integration
+@pytest.mark.skipif(not HAVE_ERT, reason="Requires ERT to be installed")
+def test_ert_forward_model(tmp_path):
+    """Test that the ERT hook can run on a mocked case"""
+    # pylint: disable=redefined-outer-name
+    # pylint: disable=unused-argument
+    os.chdir(tmp_path)
+
+    ert_config_fname = "test_params2csv.ert"
+    ert_config = [
+        "QUEUE_SYSTEM LOCAL",
+        "NUM_REALIZATIONS 1",
+        "RUNPATH <CONFIG_PATH>",
+        f"FORWARD_MODEL PARAMS2CSV(<PARAMETERFILES>={ert_config_fname})",
+    ]
+    Path(ert_config_fname).write_text("\n".join(ert_config), encoding="utf8")
+    subprocess.run(["ert", "test_run", ert_config_fname], check=True)
+    dframe = pd.read_csv("parameters.csv")
+    assert not dframe.empty
+    assert "QUEUE_SYSTEM" in dframe
+    assert "filename" in dframe
+
+
+@pytest.mark.integration
+@pytest.mark.skipif(not HAVE_ERT, reason="Requires ERT to be installed")
+def test_ert_forward_model_filename_column(tmp_path):
+    """Test that the ERT hook can run on a mocked case"""
+    # pylint: disable=redefined-outer-name
+    # pylint: disable=unused-argument
+    os.chdir(tmp_path)
+
+    ert_config_fname = "test_params2csv.ert"
+    ert_config = [
+        "QUEUE_SYSTEM LOCAL",
+        "NUM_REALIZATIONS 1",
+        "RUNPATH <CONFIG_PATH>",
+        f"FORWARD_MODEL PARAMS2CSV(<PARAMETERFILES>={ert_config_fname},"
+        "<FILENAMECOLUMN>=SOURCE_FILE)",
+    ]
+    Path(ert_config_fname).write_text("\n".join(ert_config), encoding="utf8")
+    subprocess.run(["ert", "test_run", ert_config_fname], check=True)
+    dframe = pd.read_csv("parameters.csv")
+    assert not dframe.empty
+    assert "QUEUE_SYSTEM" in dframe
+    assert "filename" not in dframe
+    assert "SOURCE_FILE" in dframe
+
+
+@pytest.mark.integration
+@pytest.mark.skipif(not HAVE_ERT, reason="Requires ERT to be installed")
+def test_ert_workflow(tmp_path):
+    """Test that PARAMS2CSV can be run as an ERT workflow/plugin"""
+    os.chdir(tmp_path)
+
+    realizations = 3
+    for i in range(realizations):
+        Path(f"realization-{i}/iter-0").mkdir(parents=True)
+        Path(f"realization-{i}/iter-0/parameters.txt").write_text(
+            f"real\t{i}", encoding="utf8"
+        )
+
+    Path("PARAMS2CSV_ITER0").write_text(
+        (
+            'PARAMS2CSV "-o" <CONFIG_PATH>/parameters.csv '
+            "<CONFIG_PATH>/realization-*/iter-0/parameters.txt"
+        )
+    )
+
+    ert_config_fname = "test_params2csv.ert"
+    ert_config = [
+        "QUEUE_SYSTEM LOCAL",
+        "NUM_REALIZATIONS 1",
+        "RUNPATH <CONFIG_PATH>",
+        "",
+        "LOAD_WORKFLOW PARAMS2CSV_ITER0",
+    ]
+    Path(ert_config_fname).write_text("\n".join(ert_config), encoding="utf8")
+    subprocess.run(
+        ["ert", "workflow", "PARAMS2CSV_ITER0", ert_config_fname], check=True
+    )
+
+    dframe = pd.read_csv("parameters.csv")
+    assert not dframe.empty
+    assert "Realization" in dframe
+    assert "real" in dframe
+    assert len(dframe.index) == realizations
