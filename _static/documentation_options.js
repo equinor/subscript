32456723cbc35361bfa152458a98e03f71e7e277
subscript-github-action <subscript-github-action>
2023-07-21 09:14:25 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16.6.dev35+g49d8580c',
+    VERSION: '0.16.6.dev36+g29e2154f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
