5078bc973f2f8a93d0da1cab3243abf67b62954d
Daniel Berge Sollien <daniel.sollien@gmail.com>
2024-01-11 15:44:53 +0100

    Improve tests

--- tests/test_fmuobs_w_field_data.py
+++ tests/test_fmuobs_w_field_data.py
@@ -1,37 +1,50 @@
+import re
 from pathlib import Path
-from subscript.fmuobs.fmuobs import autoparse_file, ertobs2df, df2obsdict
+import yaml
+from subscript.fmuobs.fmuobs import autoparse_file, df2obsdict
 from pandas import DataFrame
 import pytest
+
+from ._common_fmuobs import (
+    _find_observation_file,
+    _assert_compulsories_are_correct,
+    _compare_to_results_in_file,
+)
 
 TEST_DATA = "testdata_fmuobs/"
 
 
-def _find_observation_file(file_path):
-    """Return path to observation file
-
-    Args:
-        file_path (str): ert observation file to parse
-
-    Returns:
-        PosixPath: the path to observation file
-    """
-    obs_file_path = Path(__file__).parent / TEST_DATA / file_path
-    if not obs_file_path.exists():
-        raise FileNotFoundError(f"Cannot find observation file {obs_file_path}")
-    return obs_file_path
-
-
+@pytest.mark.integration
 @pytest.mark.parametrize(
     "obs_file",
     ["drogon/drogon_wbhp_rft_wct_gor_tracer_4d_plt.obs", "snorre/all_obs_230310.txt"],
 )
 def test_from_autoparse_file_to_df2obsdict(obs_file):
-    full_data_path = _find_observation_file(obs_file)
+    """Test dictionaries produced for drogon and snorre are as expected
+
+    Args:
+        obs_file (str): ert observation file
+    """
+    full_data_path = _find_observation_file(obs_file, "")
     file_type, dataframe = autoparse_file(full_data_path)
-    print("\n", file_type)
-    print("-------")
-    print(dataframe)
     assert file_type == "ert"
     assert isinstance(dataframe, DataFrame)
     result_dict = df2obsdict(dataframe, full_data_path.parent)
-    print(result_dict)
+    assert isinstance(
+        result_dict, dict
+    ), f"Results from dfobsdict should be dictionary, but is {type(result_dict)}"
+    assert isinstance(
+        result_dict["general"], dict
+    ), "key general from df2obsdict should be dictionary"
+    for key, item in result_dict.items():
+        if key == "general":
+            continue
+        assert isinstance(
+            item, list
+        ), f"{key} is expected to be list but is {type(item)}"
+    # _compare_to_results_in_file(
+    #     result_dict,
+    #     Path(__file__).parent
+    #     / TEST_DATA
+    #     / re.sub(r"\..*", "", obs_file.split("/")[-1]),
+    # )
