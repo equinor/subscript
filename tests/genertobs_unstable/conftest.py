14c03dec898c0aa706792db174d803e8ab688937
Daniel Berge Sollien <daniel.sollien@gmail.com>
2024-03-20 15:20:01 +0100

    Add logging for conftest

--- tests/genertobs_unstable/conftest.py
+++ tests/genertobs_unstable/conftest.py
@@ -1,10 +1,15 @@
 """Fixtures for genertobs"""
 
+import logging
 from pathlib import Path
 import pytest
+
+logging.basicConfig(level="DEBUG")
+LOGGER = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="session", name="drogon_project")
 def _fix_drogon():
     drogon_path = Path(__file__).parent / "data/drogon"
+    LOGGER.debug("Returning %s", str(drogon_path))
     return drogon_path
