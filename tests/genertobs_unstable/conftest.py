df37e1c3dc203dcd3fde07b2b27ccce812a55f0e
Daniel Berge Sollien <daniel.sollien@gmail.com>
2024-04-02 08:34:30 +0200

    Add function for reading yaml config

--- tests/genertobs_unstable/conftest.py
+++ tests/genertobs_unstable/conftest.py
@@ -20,3 +20,11 @@ def _fix_csv_config(drogon_project):
     config_path = drogon_project / "ert/input/observations/config.csv"
     LOGGER.debug("Returning %s", str(config_path))
     return config_path
+
+
+@pytest.fixture(scope="session", name="yaml_config")
+def _fix_yaml_config():
+    config_path = Path(__file__).parent / "data/expected_results.yml"
+    string_config = str(config_path)
+    assert config_path.exists(), f"{string_config} does not exist"
+    return string_config
