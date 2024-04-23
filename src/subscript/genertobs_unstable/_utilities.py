0db0d9854af6eca91ab36f49d6c22f2d6f30d4d3
Daniel Berge Sollien <daniel.sollien@gmail.com>
2024-04-23 22:33:43 +0200

    Add functions for writing in ertobs format

--- src/subscript/genertobs_unstable/_utilities.py
+++ src/subscript/genertobs_unstable/_utilities.py
@@ -390,3 +390,125 @@ def read_obs_frame(
     logger.debug("Returning %s", obs_frame)
     obs_frame = _ensure_low_caps_columns(obs_frame)
     return obs_frame
+
+
+def write_timeseries_ertobs(obs_dict):
+    logger = logging.getLogger(__name__ + ".write_timeseries_ertobs")
+    logger.debug("%s observations to write", obs_dict)
+    obs_frames = []
+    for element in obs_dict["observations"]:
+        key = element["vector"]
+        del element["vector"]
+        obs_frame = pd.DataFrame(element)
+        obs_frame["class"] = "SUMMARY_OBSERVATION"
+        obs_frame["key"] = key + ";};"
+        order = ["class", "label", "value", "error", "date", "key"]
+        obs_frame = obs_frame[order]
+        obs_frame["value"] = "{VALUE=" + obs_frame["value"].astype(str) + ";"
+        obs_frame["error"] = "ERROR=" + obs_frame["error"].astype(str) + ";"
+        obs_frame["date"] = "DATE=" + obs_frame["date"].astype(str) + ";"
+        obs_frames.append(obs_frame)
+    obs_frames = pd.concat(obs_frames)
+    return re.sub(r" +", " ", obs_frames.to_string(header=False, index=False)) + "\n"
+
+
+def select_from_dict(keys, full_dict):
+    return {key: full_dict[key] for key in keys}
+
+
+def create_rft_ertobs_str(well_name, restart, obs_file):
+    return (
+        f"GENERAL_OBSERVATION {well_name}_OBS "
+        + "{"
+        + f"DATA={well_name}_SIM ;"
+        + f" RESTART = {restart}; "
+        + f"OBS_FILE = {obs_file}"
+        + ";};\n"
+    )
+
+
+def create_rft_gendata_str(well_name, restart):
+    return (
+        f"GENERAL_DATA {well_name}_SIM "
+        + "{"
+        + f"RESULT_FILE:RFT_{well_name}_%d"
+        + f"REPORT_STEPS:{restart}"
+    )
+
+
+def write_rft_ertobs(rft_dict, parent_folder=""):
+    logger = logging.getLogger(__name__ + ".write_rft_ertobs")
+    parent_folder = Path(parent_folder)
+    logger.debug("%s observations to write", rft_dict)
+    well_date_list = []
+    rft_ertobs = ""
+    gen_data = ""
+    try:
+        metadata = rft_dict["metadata"]
+    except KeyError:
+        logger.warning("No metadata for %s", rft_dict["name"])
+        metadata = {}
+    columns = metadata.get("columns", {"value": {"unit": "bar"}})
+    rft_format = columns["value"]["unit"]
+    valid_sat_format = ["fraction", "saturation"]
+    logger.debug("Rft format is : %s", rft_format)
+    if rft_format in valid_sat_format:
+        prefix = "saturation"
+    else:
+        prefix = "pressure"
+
+    for element in rft_dict["observations"]:
+        well_name = element["well_name"]
+        logger.debug(well_name)
+        date = element["date"]
+        restart = element["restart"]
+        obs_file = write_well_rft_files(parent_folder, prefix, element)
+        well_date_list.append([well_name, date, restart])
+        rft_ertobs += create_rft_ertobs_str(well_name, restart, obs_file)
+        gen_data += create_rft_gendata_str(well_name, restart)
+    well_date_frame = pd.DataFrame(
+        well_date_list, columns=["well_name", "date", "restart"]
+    )
+
+    well_date_frame.to_csv(
+        parent_folder / "well_date_restart.txt", index=False, header=False, sep=" "
+    )
+
+    return rft_ertobs
+
+
+def write_well_rft_files(parent_folder, prefix, element):
+    logger = logging.getLogger(__name__ + ".write_well_rft_files")
+    well_name = element["well_name"]
+    obs_file = parent_folder / f"{prefix}_{well_name}.obs"
+    position_file = parent_folder / f"{prefix}_{well_name}.txt"
+    logger.debug("Writing %s and %s", obs_file, position_file)
+    obs_frame = pd.DataFrame(select_from_dict(["value", "error"], element))
+    logger.debug("observations\n%s", obs_frame)
+    obs_frame.to_csv(obs_file, index=False, header=False, sep=" ")
+    position_frame = pd.DataFrame(
+        select_from_dict(["x", "y", "md", "tvd", "zone"], element)
+    )
+    logger.debug("positions for\n%s", position_frame)
+    position_frame.to_csv(position_file, index=False, header=False, sep=" ")
+    return obs_file
+
+
+def write_dict_to_ertobs(obs_list, parent=""):
+    logger = logging.getLogger(__name__ + ".write_dict_to_ertobs")
+    logger.debug("%s observations to write", len(obs_list))
+    obs_str = ""
+    for obs in obs_list:
+        logger.debug(obs)
+        content = obs["content"]
+        obs_str += f"--\n--{obs['name']}\n"
+        if content == "timeseries":
+            obs_str += write_timeseries_ertobs(obs)
+
+        elif content == "rft":
+            obs_str += write_rft_ertobs(obs, "")
+        else:
+            logger.warning(
+                "Currently not supporting other formats than timeseries and rft"
+            )
+    return obs_str
