9c5bc7848d6046af8cc444c2c8dd5b4b6adffc78
Daniel Berge Sollien <daniel.sollien@gmail.com>
2024-05-24 10:31:17 +0200

    Add writing of sumo_env to hook

--- src/subscript/genertobs_unstable/_writers.py
+++ src/subscript/genertobs_unstable/_writers.py
@@ -348,7 +348,7 @@ def generate_preprocessed_hook(export_path, output_folder):
 
     posixpath = Path(output_folder).resolve()
     workflow_name = posixpath / f"xhook_{stem}"
-    call = f"WF_UPLOAD_SUMO_OBS <SCRATCH>/<USER>/<CASE_DIR> {str(export_path)}"
+    call = f"WF_UPLOAD_SUMO_OBS <SCRATCH>/<USER>/<CASE_DIR> {str(export_path)} '--env' <SUMO_ENV>"
     workflow_name.write_text(add_time_stamp(call))
 
     workflow_content = f"LOAD_WORKFLOW   {workflow_name}  -- define and load workflow\n"
