4e2400627a273368a69c189ac930016fac67702e
Trine Alsos <tral@statoil.com>
2024-06-04 14:18:57 +0200

    update gravity test and add documentation

--- src/subscript/grav_subs_maps/grav_subs_maps.py
+++ src/subscript/grav_subs_maps/grav_subs_maps.py
@@ -1,21 +1,21 @@
 import argparse
 import logging
 import os
-from datetime import date
 import sys
+from datetime import date
 from pathlib import Path
-from typing import Any, Dict, List, Literal, Optional
-import yaml
-from pydantic import BaseModel, Field, FilePath, model_validator, field_validator
-from typing_extensions import Annotated
-
-from resdata.resfile import ResdataFile
-from resdata.grid import Grid
-from resdata.gravimetry import ResdataGrav, ResdataSubsidence
+from typing import Any, Dict, List, Optional
 
 import xtgeo
+import yaml
+from pydantic import BaseModel, Field, FilePath, field_validator
+from resdata.gravimetry import ResdataGrav, ResdataSubsidence
+from resdata.grid import Grid
+from resdata.resfile import ResdataFile
+from typing_extensions import Annotated
 
 import subscript
+
 logger = subscript.getLogger(__name__)
 
 # Constant for subsidence modelling, not influencing results
@@ -57,12 +57,14 @@ CATEGORY = "modelling.reservoir"
 EXAMPLES = """
 .. code-block:: console
 
- FORWARD_MODEL GRAV_SUBS_MAPS(<UNRST_FILE>=<ECLBASE>.UNRST, <GRAV_CONFIG>=grav_subs_maps.yml, <OUTPUTDIR>=share/results/maps)
+ FORWARD_MODEL GRAV_SUBS_MAPS(<UNRST_FILE>=<ECLBASE>.UNRST, <GRAV_CONFIG>=grav_subs_maps.yml, <ROOT_PATH>=<CONFIG_PATH>, <OUTPUTDIR>=share/results/maps)
 
 
 where ``ECLBASE`` is already defined in your ERT config, pointing to the flowsimulator
 basename relative to ``RUNPATH``, grav_subs_maps.yml is a YAML file defining
 the inputs and modelling parameters and ``OUTPUTDIR`` is the path to the output folder.
+``ROOT_PATH`` is optinal and defaulted to "./". This is the rooth path assumed for
+relative paths in the yml config.
 
 The directory to export maps to must exist.
 """  # noqa
@@ -72,6 +74,7 @@ class GravInput(BaseModel):
     diffdates: List[List[date]]
     seabed_map: FilePath
 
+
 class GravCalc(BaseModel):
     poisson_ratio: Annotated[float, Field(strict=True, ge=0, le=0.5)]
     coarsening: Optional[Annotated[int, Field(strict=True, ge=1)]] = None
@@ -80,18 +83,15 @@ class GravCalc(BaseModel):
     @field_validator("phases")
     @classmethod
     def check_phases(cls, phases: List[str]) -> List[str]:
-        allowed_phases = ['oil', 'gas', 'water','total']
+        allowed_phases = ["oil", "gas", "water", "total"]
         for item in phases:
-            assert (
-                item in allowed_phases
-            ), f"allowed phases are {str(allowed_phases)}"
+            assert item in allowed_phases, f"allowed phases are {str(allowed_phases)}"
         return phases
 
 
 class GravMapsConfig(BaseModel):
     input: GravInput
     calculations: GravCalc
-
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -108,11 +108,7 @@ def get_parser() -> argparse.ArgumentParser:
         formatter_class=argparse.RawTextHelpFormatter,
     )
 
-    parser.add_argument(
-        "UNRSTfile",
-        type=str,
-        help="Path to flowsimulator UNRST file"
-    )
+    parser.add_argument("UNRSTfile", type=str, help="Path to flowsimulator UNRST file")
     parser.add_argument(
         "-c",
         "-C",
@@ -120,6 +116,13 @@ def get_parser() -> argparse.ArgumentParser:
         type=str,
         help="Name of YAML config file",
         required=True,
+    )
+    parser.add_argument(
+        "-r",
+        "--root-path",
+        type=str,
+        default="./",
+        help=("Root path assumed for relative paths" " in config file."),
     )
     parser.add_argument(
         "-o",
@@ -147,38 +150,74 @@ def main() -> None:
     if not Path(args.configfile).exists():
         sys.exit("No such file:" + args.configfile)
     config = yaml.safe_load(Path(args.configfile).read_text(encoding="utf8"))
-    cfg = GravMapsConfig.model_validate(config).model_dump()
+
+    # cfg = GravMapsConfig.model_validate(config).model_dump()
 
     if not Path(args.outputdir).exists():
         sys.exit("Output folder does not exist:" + args.outputdir)
     if not Path(args.UNRSTfile).exists():
         sys.exit("UNRST file does not exist:" + args.UNRSTfile)
 
-    main_gravmaps(args.UNRSTfile, cfg, Path(args.outputdir))
+    main_gravmaps(args.UNRSTfile, config, Path(args.root_path), Path(args.outputdir))
 
 
-def main_gravmaps(unrst_file: str, cfg: Dict[str, Any], output_folder: Path) -> None:
+def prepend_root_path_to_relative_files(
+    cfg: Dict[str, Any], root_path: Path
+) -> Dict[str, Any]:
+    """Prepend root_path to relative files found paths in a configuration
+    dictionary.
+
+    Note: This function is before prior to validation of the configuration!
+
+    Will look for filename in the key "input["seabed_map"]"
+
+    Args:
+        cfg: grav_subs_maps configuration dictionary
+        root_path: An relative or absolute path to be prepended
+
+    Returns:
+        Modified configuration for interp_relperm
+    """
+    if (
+        "input" in cfg
+        and "seabed_map" in cfg["input"]
+        and not os.path.isabs(cfg["input"]["seabed_map"])
+    ):
+        cfg["input"]["seabed_map"] = str(root_path / Path(cfg["input"]["seabed_map"]))
+    return cfg
+
+
+def main_gravmaps(
+    unrst_file: str,
+    config: Dict[str, Any],
+    root_path: Optional[Path],
+    output_folder: Path,
+) -> None:
     """
     Process a configuration, model gravity and subsidence surfaces and write to disk.
 
     Args:
         resdata: Path to flow simulation UNRST file
-        cfg: Configuration for modelling
+        config: Configuration for modelling
     """
 
+    if root_path is not None:
+        config = prepend_root_path_to_relative_files(config, root_path)
+
+    cfg = GravMapsConfig.model_validate(config).model_dump()
+
     # Read inputs and calculation parameters
-    input_diffdates = cfg['input']['diffdates']
-    map_template = cfg['input']['seabed_map']
-    coarsening = cfg['calculations']['coarsening']
-    phases = cfg['calculations']['phases']
-    poisson_ratio = cfg['calculations']['poisson_ratio']
+    input_diffdates = cfg["input"]["diffdates"]
+    map_template = cfg["input"]["seabed_map"]
+    coarsening = cfg["calculations"]["coarsening"]
+    phases = cfg["calculations"]["phases"]
+    poisson_ratio = cfg["calculations"]["poisson_ratio"]
 
     # Read seabed map and coarsen
     seabed = xtgeo.surface_from_file(map_template)
     seabed.coarsen(coarsening)
 
-
-    if isinstance(unrst_file,str):
+    if isinstance(unrst_file, str):
         restart_file = unrst_file[:-6] + ".UNRST"
         egrid_file = unrst_file[:-6] + ".EGRID"
         init_file = unrst_file[:-6] + ".INIT"
@@ -192,73 +231,88 @@ def main_gravmaps(unrst_file: str, cfg: Dict[str, Any], output_folder: Path) -> 
     for i, restart_date in enumerate(rest.dates):
         restart_index[restart_date.strftime("%Y%m%d")] = i
 
-    diffdates=[]
+    diffdates = []
     # Convert dates from datetime format to strings
     logger.info("Will do modelling for diffdates: ")
     for diffdate in input_diffdates:
-        diff = [diffdate[0].strftime("%Y%m%d"),diffdate[1].strftime("%Y%m%d")]
+        diff = [diffdate[0].strftime("%Y%m%d"), diffdate[1].strftime("%Y%m%d")]
         diffdates.append(diff)
-        logger.info(
-            f'{diffdate[0]}_{diffdate[1]}'
-            )
+        logger.info(f"{diffdate[0]}_{diffdate[1]}")
 
-    grav = ResdataGrav(grid,init)
+    grav = ResdataGrav(grid, init)
     subsidence = ResdataSubsidence(grid, init)
 
-    added_dates=[]
+    added_dates = []
 
     for diffdate in diffdates:
-        for date in diffdate: # base and monitor
+        for singledate in diffdate:  # base and monitor
             rsb = rest.restartView(0)
-            if date not in added_dates:
-                if date in restart_index.keys():
-                    rsb = rest.restartView( restart_index[date])
-                    grav.add_survey_RFIP(date, rsb)
-                    subsidence.add_survey_PRESSURE(date, rsb)
-                    added_dates.append(date)
+            if singledate not in added_dates:
+                if singledate in restart_index:
+                    rsb = rest.restartView(restart_index[singledate])
+                    grav.add_survey_RFIP(singledate, rsb)
+                    subsidence.add_survey_PRESSURE(singledate, rsb)
+                    added_dates.append(singledate)
                 else:
                     logger.error(
-                        f"Date {date} specified but not"
-                        "found in UNRST file."
+                        f"Date {singledate} specified but not" "found in UNRST file."
                     )
                     sys.exit(1)
-    phase_code = {'oil':1, 'gas':2, 'water':4, 'total':7}
+    phase_code = {"oil": 1, "gas": 2, "water": 4, "total": 7}
 
     # Gravity
     for diffdate in diffdates:
         for phase in phases:
-            logger.info(f'Calculating delta gravity map from {phase} for {diffdate[0]}_{diffdate[1]}')
+            logger.info(
+                f"Calculating delta gravity map from {phase} "
+                "for {diffdate[0]}_{diffdate[1]}"
+            )
             dgsim = seabed.copy()
             df_dgsim = dgsim.get_dataframe()
             dgsim_series = []
-            for index,row in df_dgsim.iterrows():
-                dgsim_series.append(grav.eval(diffdate[1], diffdate[0],
-                                              (row['X_UTME'], row['Y_UTMN'], row['VALUES']),
-                                              phase_mask=phase_code[phase]))
+            for index, row in df_dgsim.iterrows():
+                dgsim_series.append(
+                    grav.eval(
+                        diffdate[1],
+                        diffdate[0],
+                        (row["X_UTME"], row["Y_UTMN"], row["VALUES"]),
+                        phase_mask=phase_code[phase],
+                    )
+                )
             dgsim.values = dgsim_series
-            filename = PREFIX_GRAVSURF + phase+"--" + diffdate[0] + "_" + diffdate[1]+".gri"
+            filename = (
+                PREFIX_GRAVSURF
+                + phase
+                + "--"
+                + diffdate[0]
+                + "_"
+                + diffdate[1]
+                + ".gri"
+            )
             dgsim.to_file(os.path.join(output_folder, filename))
 
     # Subsidence
     for diffdate in diffdates:
-        logger.info(f'Calculating subsidence map for {diffdate[0]}_{diffdate[1]}')
+        logger.info(f"Calculating subsidence map for {diffdate[0]}_{diffdate[1]}")
         dzsim = seabed.copy()
         df_dzsim = dzsim.get_dataframe()
         dzsim_series = []
-        for index,row in df_dzsim.iterrows():
+        for index, row in df_dzsim.iterrows():
             dzsim_series.append(
                 subsidence.eval_geertsma_rporv(
-                    diffdate[1], diffdate[0],
-                    (row['X_UTME'], row['Y_UTMN'], row['VALUES']),
-                    DUMMY_YOUNGS, poisson_ratio, row['VALUES']
-                    ))
+                    diffdate[1],
+                    diffdate[0],
+                    (row["X_UTME"], row["Y_UTMN"], row["VALUES"]),
+                    DUMMY_YOUNGS,
+                    poisson_ratio,
+                    row["VALUES"],
+                )
+            )
 
-        dzsim.values = [i*100 for i in dzsim_series]  # From m to cms
+        dzsim.values = [i * 100 for i in dzsim_series]  # From m to cms
 
-        filename = PREFIX_SUBSSURF +"--" + diffdate[0] + "_" + diffdate[1]+".gri"
+        filename = PREFIX_SUBSSURF + "--" + diffdate[0] + "_" + diffdate[1] + ".gri"
         dzsim.to_file(os.path.join(output_folder, filename))
-
-
 
     logger.info(
         "Done; All gravity and subsidence maps written to folder: %s",
---
+++ tests/test_grav_subs_maps.py
@@ -0,0 +1,184 @@
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+import pytest
+import yaml
+from pydantic import ValidationError
+from subscript.grav_subs_maps import grav_subs_maps
+from subscript.grav_subs_maps.grav_subs_maps import GravMapsConfig
+
+TESTDATA = Path(__file__).absolute().parent / "testdata_gravity"
+
+
+def test_prepend_root_path():
+    """Test that we need to prepend with root-path"""
+    cfg_file = TESTDATA / "grav_subs_maps_hist.yml"
+
+    cfg = yaml.safe_load(cfg_file.read_text(encoding="utf8"))
+
+    with pytest.raises(ValidationError):
+        GravMapsConfig(**cfg)
+
+    cfg_with_rootpath = grav_subs_maps.prepend_root_path_to_relative_files(
+        cfg, TESTDATA
+    )
+    GravMapsConfig(**cfg_with_rootpath)
+
+    # When root-path is prepended (with an absolute part) it should not
+    # matter if we reapply:
+    cfg_with_double_rootpath = grav_subs_maps.prepend_root_path_to_relative_files(
+        cfg_with_rootpath, TESTDATA
+    )
+    GravMapsConfig(**cfg_with_double_rootpath)
+
+
+@pytest.fixture(name="res_data")
+def fixture_res_data(tmp_path):
+    """Prepare a data directory with Eclipse binary output"""
+
+    resdatadest = tmp_path / "resdata"
+    shutil.copytree(TESTDATA, resdatadest, copy_function=os.symlink)
+    cwd = os.getcwd()
+    os.chdir(resdatadest)
+
+    try:
+        yield
+
+    finally:
+        os.chdir(cwd)
+
+
+@pytest.mark.parametrize(
+    "dictupdates, expected_error",
+    [
+        (
+            {
+                "calculations": {
+                    "poisson_ratio": 1,
+                    "coarsening": 8,
+                    "phases": ["gas", "oil", "water", "total"],
+                }
+            },
+            "Input should be less than",
+        ),
+        (
+            {
+                "calculations": {
+                    "poisson_ratio": -1,
+                    "coarsening": 8,
+                    "phases": ["gas", "oil", "water", "total"],
+                }
+            },
+            "Input should be greater than",
+        ),
+        (
+            {
+                "calculations": {
+                    "poisson_ratio": 0.45,
+                    "coarsening": 0,
+                    "phases": ["gas", "oil", "water", "total"],
+                }
+            },
+            "Input should be greater than",
+        ),
+        (
+            {
+                "calculations": {
+                    "poisson_ratio": 0.45,
+                    "coarsening": 8,
+                    "phases": ["wrong_phase"],
+                }
+            },
+            "Assertion failed, allowed phases are",
+        ),
+        (
+            {
+                "calculations": {
+                    "poisson_ratio": 0.45,
+                    "phases": ["gas", "oil", "water", "total"],
+                }
+            },
+            None,
+        ),
+        (
+            {
+                "calculations": {
+                    "coarsening": 8,
+                    "phases": ["gas", "oil", "water", "total"],
+                }
+            },
+            "Field required",
+        ),
+        (
+            {
+                "input": {
+                    "diffdates": [["2020-7-01", "2018-01-01"]],
+                    "seabed_map": "./seabed.gri",
+                }
+            },
+            "Input should be a valid date",
+        ),
+        (
+            {
+                "input": {
+                    "diffdates": [["2020-07-01", "2018-01-01"]],
+                    "seabed_map": "./no_seabed.gri",
+                }
+            },
+            "Path does not point to a file",
+        ),
+    ],
+)
+def test_config_errors(dictupdates, expected_error):
+    """Test for error in configuration file"""
+
+    os.chdir(TESTDATA)
+    cfg = {
+        "input": {
+            "diffdates": [["2020-07-01", "2018-01-01"]],
+            "seabed_map": "./seabed.gri",
+        },
+        "calculations": {
+            "poisson_ratio": 0.45,
+            "coarsening": 8,
+            "phases": ["gas", "oil", "water", "total"],
+        },
+    }
+    cfg.update(dictupdates)
+
+    if expected_error is not None:
+        with pytest.raises(ValidationError) as validation_error:
+            GravMapsConfig(**cfg)
+        assert expected_error in str(validation_error)
+    else:
+        GravMapsConfig(**cfg)
+
+
+@pytest.mark.integration
+def test_integration():
+    """Test that endpoint is installed"""
+    assert subprocess.check_output(["grav_subs_maps", "-h"])
+
+
+@pytest.mark.integration
+def test_main(res_data, mocker):
+    """Test invocation from command line"""
+
+    assert subprocess.check_output(["grav_subs_maps", "-h"])
+
+    test_cfg = "grav_subs_maps_hist.yml"
+    test_resfile = "HIST.UNRST"
+
+    mocker.patch(
+        "sys.argv",
+        [__file__, test_resfile, "--configfile", str(test_cfg), "--outputdir", "./"],
+    )
+    grav_subs_maps.main()
+
+    assert Path("all--delta_gravity_total--20200701_20180101.gri").exists()
+    assert Path("all--delta_gravity_gas--20200701_20180101.gri").exists()
+    assert Path("all--delta_gravity_oil--20200701_20180101.gri").exists()
+    assert Path("all--delta_gravity_water--20200701_20180101.gri").exists()
+    assert Path("all--subsidence--20200701_20180101.gri").exists()
