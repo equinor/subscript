f206b665ff76fd19c62338dcde4509fdf844a063
lilbe66 <lilbe@equinor.com>
2024-06-19 10:00:52 +0200

    New fipname option

--- tests/test_prtvol2csv.py
+++ tests/test_prtvol2csv.py
@@ -17,6 +17,7 @@ from subscript.prtvol2csv import prtvol2csv
 from .utils import run_simulator
 
 TESTDATADIR = Path(__file__).absolute().parent / "data/reek/eclipse/model"
+TEST_PRT_DATADIR = Path(__file__).absolute().parent / "testdata_prtvol2csv"
 
 DROGON_TEST_DATA_DIR = Path(__file__).absolute().parent / "data/drogon/eclipse"
 
@@ -54,7 +55,7 @@ def test_valid_flow_output(simulator, tmp_path, mocker):
     assert dframe.shape == (21, 13)
 
 
-def test_currently_in_place_from_prt(tmp_path):
+def test_reservoir_volumes_from_prt(tmp_path):
     """Test parsing of PRT to find currently in place"""
     os.chdir(tmp_path)
     Path("FOO.PRT").write_text(
@@ -178,6 +179,355 @@ def test_prtvol2csv(tmp_path, mocker):
             },
         }
     )
+    expected["FIPNAME"] = "FIPNUM"
+    pd.testing.assert_frame_equal(dframe, expected)
+
+
+def test_fipxxx(tmp_path, mocker):
+    """Test invocation from command line"""
+    prtfile = TEST_PRT_DATADIR / "DROGON_FIPZON.PRT"
+
+    os.chdir(tmp_path)
+
+    # Test for FIPZON:
+    with pytest.warns(FutureWarning, match="Output directories"):
+        mocker.patch(
+            "sys.argv", ["prtvol2csv", "--fipname", "FIPZON", "--debug", str(prtfile)]
+        )
+        prtvol2csv.main()
+    dframe = pd.read_csv("share/results/volumes/simulator_volume_fipnum.csv")
+
+    expected = pd.DataFrame.from_dict(
+        {
+            "FIPNUM": {0: 1, 1: 2, 2: 3},
+            "STOIIP_OIL": {0: 19549844.0, 1: 11560982.0, 2: 13683668.0},
+            "ASSOCIATEDOIL_GAS": {0: 102008.0, 1: 65583.0, 2: 6790.0},
+            "STOIIP_TOTAL": {0: 19651853.0, 1: 11626565.0, 2: 13690458.0},
+            "WIIP_TOTAL": {0: 153820626.0, 1: 136545137.0, 2: 166492503.0},
+            "GIIP_GAS": {0: 664018338.0, 1: 425072072.0, 2: 43915280.0},
+            "ASSOCIATEDGAS_OIL": {0: 2764802197.0, 1: 1647942723.0, 2: 1962726869.0},
+            "GIIP_TOTAL": {0: 3428820535.0, 1: 2073014795.0, 2: 2006642149.0},
+            "PORV_TOTAL": {0: 190002679.0, 1: 159669348.0, 2: 192088820.0},
+            "HCPV_OIL": {0: 28079757.0, 1: 16649101.0, 2: 19746072.0},
+            "WATPV_TOTAL": {0: 159065528.0, 1: 141192398.0, 2: 172153976.0},
+            "HCPV_GAS": {0: 2857395.0, 1: 1827848.0, 2: 188773.0},
+            "HCPV_TOTAL": {0: 30937151.0, 1: 18476950.0, 2: 19934844.0},
+        }
+    )
+    expected["FIPNAME"] = "FIPZON"
+    pd.testing.assert_frame_equal(dframe, expected)
+
+    # Test for FIPNUM:
+    mocker.patch(
+        "sys.argv", ["prtvol2csv", "--fipname", "FIPNUM", "--debug", str(prtfile)]
+    )
+    prtvol2csv.main()
+    dframe = pd.read_csv("share/results/volumes/simulator_volume_fipnum.csv")
+
+    expected = pd.DataFrame.from_dict(
+        {
+            "FIPNUM": {
+                0: 1,
+                1: 2,
+                2: 3,
+                3: 4,
+                4: 5,
+                5: 6,
+                6: 7,
+                7: 8,
+                8: 9,
+                9: 10,
+                10: 11,
+                11: 12,
+                12: 13,
+                13: 14,
+                14: 15,
+                15: 16,
+                16: 17,
+                17: 18,
+                18: 19,
+                19: 20,
+                20: 21,
+            },
+            "STOIIP_OIL": {
+                0: 1885827.0,
+                1: 7179776.0,
+                2: 2366384.0,
+                3: 213993.0,
+                4: 986924.0,
+                5: 6916940.0,
+                6: 0.0,
+                7: 383716.0,
+                8: 3555202.0,
+                9: 1092897.0,
+                10: 361553.0,
+                11: 958614.0,
+                12: 5209000.0,
+                13: 0.0,
+                14: 39328.0,
+                15: 4296946.0,
+                16: 381370.0,
+                17: 649478.0,
+                18: 1210916.0,
+                19: 7105630.0,
+                20: 0.0,
+            },
+            "ASSOCIATEDOIL_GAS": {
+                0: 0.0,
+                1: 0.0,
+                2: 0.0,
+                3: 102008.0,
+                4: 0.0,
+                5: 0.0,
+                6: 0.0,
+                7: 0.0,
+                8: 0.0,
+                9: 0.0,
+                10: 65583.0,
+                11: 0.0,
+                12: 0.0,
+                13: 0.0,
+                14: 0.0,
+                15: 0.0,
+                16: 0.0,
+                17: 6790.0,
+                18: 0.0,
+                19: 0.0,
+                20: 0.0,
+            },
+            "STOIIP_TOTAL": {
+                0: 1885827.0,
+                1: 7179776.0,
+                2: 2366384.0,
+                3: 316001.0,
+                4: 986924.0,
+                5: 6916940.0,
+                6: 0.0,
+                7: 383716.0,
+                8: 3555202.0,
+                9: 1092897.0,
+                10: 427136.0,
+                11: 958614.0,
+                12: 5209000.0,
+                13: 0.0,
+                14: 39328.0,
+                15: 4296946.0,
+                16: 381370.0,
+                17: 656268.0,
+                18: 1210916.0,
+                19: 7105630.0,
+                20: 0.0,
+            },
+            "WIIP_TOTAL": {
+                0: 86129188.0,
+                1: 13001181.0,
+                2: 11374802.0,
+                3: 4239412.0,
+                4: 1738280.0,
+                5: 6692494.0,
+                6: 30645268.0,
+                7: 90005529.0,
+                8: 3479272.0,
+                9: 11342135.0,
+                10: 4753832.0,
+                11: 1290225.0,
+                12: 4277308.0,
+                13: 21396835.0,
+                14: 90070621.0,
+                15: 31268180.0,
+                16: 4815628.0,
+                17: 1772680.0,
+                18: 1135674.0,
+                19: 8929238.0,
+                20: 28500482.0,
+            },
+            "GIIP_GAS": {
+                0: 0.0,
+                1: 0.0,
+                2: 0.0,
+                3: 664018338.0,
+                4: 0.0,
+                5: -0.0,
+                6: 0.0,
+                7: 0.0,
+                8: 0.0,
+                9: 0.0,
+                10: 425072072.0,
+                11: 0.0,
+                12: 0.0,
+                13: 0.0,
+                14: 0.0,
+                15: 0.0,
+                16: -0.0,
+                17: 43915280.0,
+                18: 0.0,
+                19: 0.0,
+                20: 0.0,
+            },
+            "ASSOCIATEDGAS_OIL": {
+                0: 265562206.0,
+                1: 1011056014.0,
+                2: 333234206.0,
+                3: 41927638.0,
+                4: 138978686.0,
+                5: 974043447.0,
+                6: 0.0,
+                7: 54034880.0,
+                8: 500643587.0,
+                9: 153901737.0,
+                10: 70839116.0,
+                11: 134992033.0,
+                12: 733531369.0,
+                13: 0.0,
+                14: 5538130.0,
+                15: 605095985.0,
+                16: 53704503.0,
+                17: 127252257.0,
+                18: 170521130.0,
+                19: 1000614864.0,
+                20: 0.0,
+            },
+            "GIIP_TOTAL": {
+                0: 265562206.0,
+                1: 1011056014.0,
+                2: 333234206.0,
+                3: 705945976.0,
+                4: 138978686.0,
+                5: 974043447.0,
+                6: 0.0,
+                7: 54034880.0,
+                8: 500643587.0,
+                9: 153901737.0,
+                10: 495911188.0,
+                11: 134992033.0,
+                12: 733531369.0,
+                13: 0.0,
+                14: 5538130.0,
+                15: 605095985.0,
+                16: 53704503.0,
+                17: 171167537.0,
+                18: 170521130.0,
+                19: 1000614864.0,
+                20: 0.0,
+            },
+            "PORV_TOTAL": {
+                0: 91773791.0,
+                1: 23740624.0,
+                2: 15155989.0,
+                3: 7590653.0,
+                4: 3213590.0,
+                5: 16845621.0,
+                6: 31682411.0,
+                7: 93624776.0,
+                8: 8695271.0,
+                9: 13295371.0,
+                10: 7333299.0,
+                11: 2708950.0,
+                12: 11895464.0,
+                13: 22116217.0,
+                14: 93188693.0,
+                15: 38493502.0,
+                16: 5526328.0,
+                17: 3079661.0,
+                18: 2910560.0,
+                19: 19424798.0,
+                20: 29465279.0,
+            },
+            "HCPV_OIL": {
+                0: 2704441.0,
+                1: 10294749.0,
+                2: 3393024.0,
+                3: 348455.0,
+                4: 1415803.0,
+                5: 9923285.0,
+                6: 0.0,
+                7: 550246.0,
+                8: 5096855.0,
+                9: 1566877.0,
+                10: 588753.0,
+                11: 1374733.0,
+                12: 7471637.0,
+                13: 0.0,
+                14: 56393.0,
+                15: 6159286.0,
+                16: 546733.0,
+                17: 1057554.0,
+                18: 1736084.0,
+                19: 10190021.0,
+                20: 0.0,
+            },
+            "WATPV_TOTAL": {
+                0: 89069350.0,
+                1: 13445875.0,
+                2: 11762965.0,
+                3: 4384803.0,
+                4: 1797787.0,
+                5: 6922336.0,
+                6: 31682411.0,
+                7: 93074529.0,
+                8: 3598416.0,
+                9: 11728494.0,
+                10: 4916698.0,
+                11: 1334217.0,
+                12: 4423827.0,
+                13: 22116217.0,
+                14: 93132300.0,
+                15: 32334216.0,
+                16: 4979595.0,
+                17: 1833334.0,
+                18: 1174475.0,
+                19: 9234777.0,
+                20: 29465279.0,
+            },
+            "HCPV_GAS": {
+                0: 0.0,
+                1: 0.0,
+                2: 0.0,
+                3: 2857395.0,
+                4: 0.0,
+                5: 0.0,
+                6: 0.0,
+                7: 0.0,
+                8: 0.0,
+                9: 0.0,
+                10: 1827848.0,
+                11: 0.0,
+                12: 0.0,
+                13: 0.0,
+                14: 0.0,
+                15: 0.0,
+                16: 0.0,
+                17: 188773.0,
+                18: 0.0,
+                19: 0.0,
+                20: 0.0,
+            },
+            "HCPV_TOTAL": {
+                0: 2704441.0,
+                1: 10294749.0,
+                2: 3393024.0,
+                3: 3205850.0,
+                4: 1415803.0,
+                5: 9923285.0,
+                6: 0.0,
+                7: 550246.0,
+                8: 5096855.0,
+                9: 1566877.0,
+                10: 2416602.0,
+                11: 1374733.0,
+                12: 7471637.0,
+                13: 0.0,
+                14: 56393.0,
+                15: 6159286.0,
+                16: 546733.0,
+                17: 1246327.0,
+                18: 1736084.0,
+                19: 10190021.0,
+                20: 0.0,
+            },
+        }
+    )
+    expected["FIPNAME"] = "FIPNUM"
     pd.testing.assert_frame_equal(dframe, expected)
 
 
@@ -199,16 +549,21 @@ def test_find_prtfile(tmp_path):
 
 
 def test_prtvol2df(tmp_path):
-    """Test concatenation of dtaframes with volumes"""
+    """Test concatenation of dataframes with volumes"""
     simv = pd.DataFrame([{"STOIIP_OIL": 1000}], index=[1])
     resv = pd.DataFrame([{"PORV_TOTAL": 1000}], index=[1])
 
     # This function is simple concatenation horizontally:
     volumes = prtvol2csv.prtvol2df(simv, resv)
-    pd.testing.assert_frame_equal(
-        volumes, pd.DataFrame([{"STOIIP_OIL": 1000, "PORV_TOTAL": 1000}], index=[1])
+
+    test_df = pd.DataFrame(
+        [{"STOIIP_OIL": 1000, "PORV_TOTAL": 1000, "FIPNAME": "FIPNUM"}], index=[1]
     )
-    # Index is [1] implicitly, and refers to FIPNUM.
+    # Here index is [1], and refers to FIPNUM.
+    test_df.index.name = "FIPNUM"
+
+    pd.testing.assert_frame_equal(volumes, test_df)
+
     assert "REGION" not in prtvol2csv.prtvol2df(simv, resv, FipMapper())
     assert "ZONE" not in prtvol2csv.prtvol2df(simv, resv, FipMapper())
 
