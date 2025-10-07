61833155eaf502d5a2f2d568e04a37df7892878a
subscript-github-action <subscript-github-action>
2025-10-07 08:19:16 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.14.3.dev4+g9c2790af0',
+    VERSION: '1.14.3.dev5+gb225db03a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
